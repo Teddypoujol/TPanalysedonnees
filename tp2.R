4b02e6f857f5822da6868ceb08cc29f95e023e3d
Teddypoujol <45623619+Teddypoujol@users.noreply.github.com>
2019-05-14 17:50:13 +0200

    TP2 presque finis

--- tp2.R
+++ tp2.R
@@ -1,20 +1,27 @@
+
+
+# 1.Tracez en dimension 3 le nuage de 10 points.
 library(scatterplot3d)
 library(plot3D)
 library(rgl)
-individus <- read.delim("/amuhome/p17027265/Bureau/DataAnalysis-master/data1TP2.txt")
+#individus <- read.delim("/amuhome/p17027265/Bureau/DataAnalysis-master/data1TP2.txt")
+individus <- read.delim("/home/raccoon/Desktop/data1TP2.txt")
 individus <- na.omit(individus)
 #gens <- subset(individus, select = c(Stature, Poids, Taille))
 A <- individus[,1:3]
 scatterplot3d(A)
 
+#2. Écrivez le tableau centré B, et la matrice de covariance V.
 B <- scale(A, center = TRUE, scale = FALSE)
 B
 V <- cov(A)
 V
 
+#3. Déterminez la représentation spectrale (valeurs propres et vecteurs propres de V).
 valeurP <- eigen( V)$values
 vecteurP <- eigen( V)$vectors
 
+#4. Indiquez les axes principaux (dans l’ordre).
 #https://bioinfo-fr.net/lanalyse-en-composantes-principales-avec-r#exemple
 pca <- prcomp(A)
 pca$rotation
@@ -27,16 +34,19 @@ proporVariance
 varianceTotale
 plot(pca)
 
+# 5. Générez le tableau C en multipliant B par les vecteurs propres de V.
+# *vérifiez vos nouvelles données générées avec la fonction princomp(A)$scores
 GenererTableauC <- function(B,vp) {
   # C <- matrix(nrow = 10,ncol=3);
   C <- B  %*%  vp 
   return(C) 
 }
 
-GenererTableauC(B,vecteurP)
+C <- GenererTableauC(B,vecteurP)
+C
 princomp(A)$scores
 
-#6 
+#6. Observez en dimension 3 le nuage de points avec tracé du premier axe principal.
 
 pc1 <- pca$rotation[,1]
 
@@ -60,5 +70,17 @@ traceAxe(A,pc1)
 
 plt<-scatterplot3d(pca$rotation)
 plt$points3d(pc1, type="l", col="blue", lwd=2)
+
+# 7. Représentez le nuage de points en dimension 2, projetés des points de départ sur le plan formé des
+# deux premiers axes principaux.
+
+P = matrix(c(C[,1],C[,2]), ncol=2)
+plot(P[,1], P[,2], main="Nuage de Point", xlab="Stature", ylab="Poids", pch=19)
+  
+plot(C[,1],C[,2])
+
+# 8. Interprétez les résultats obtenus.
+
+# La stature est l'élément le plus important, compte tenu des vlp.
 
 
